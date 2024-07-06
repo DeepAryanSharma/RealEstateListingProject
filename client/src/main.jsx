9d1fddda92f5a10de33b214af90e62c95f135469
Deep Aryan Sharma <124386531+DeepAryanSharma@users.noreply.github.com>
2024-07-06 20:33:56 +0530

    Add files via upload

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-q21w41examuigafr.us.auth0.com"
-      clientId="cQXD4SXqTncIeYFeP0x56BdPWZeX4Spy"
+      domain="dev-ythypufsxiy5x0p1.us.auth0.com"
+      clientId="PmyNztS9kgwG3bKn2KyScov23C7PJted"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173/",
+        redirect_uri: "https://real-estate-listing-project-ten.vercel.app/",
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
